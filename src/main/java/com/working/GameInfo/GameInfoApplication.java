97dad8dca3976408aad9f253812ab039c8f463e7
ccpink <54963606+ccpink@users.noreply.github.com>
2022-10-24 13:36:48 -0300

    First commit start of rest api.

---
+++ src/main/java/com/working/GameInfo/GameInfoApplication.java
@@ -0,0 +1,13 @@
+package com.working.GameInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameInfoApplication.class, args);
+	}
+
+}
